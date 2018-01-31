c67ac6b4f183c84ddd30aeb78af0eb5077aad638
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2018-01-30 20:49:45 -0500

    remove commented out code

--- worker/runLocalGame.py
+++ worker/runLocalGame.py
@@ -7,19 +7,10 @@ def execGame():
     # setupBots()
     bots = []
 
-    bot1 = LocalBot("bot1", 1)
-    bot2 = LocalBot("bot2", 2)
-    # for i, arg in enumerate(sys.argv[1:]):
-    #     bots.append(LocalBot(arg, i))
-    bots.append(bot1)
-    bots.append(bot2)
+    for i, arg in enumerate(sys.argv[1:]):
+        bots.append(LocalBot(arg, i))
 
     game.start(bots)
-    # try:
-    #     game.start(bots)
-    # except Exception as err:
-    #     print("GAME ERR")
-    #     print(err)
 
     # get results from game and post to server
 
