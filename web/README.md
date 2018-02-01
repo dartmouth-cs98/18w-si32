c54f2cdf0c8df15d04a7ff38082b18a1bdf3b42c
Gabe Boning <gboning@gmail.com>
2018-02-01 15:04:14 -0500

    web docker base image w/ webpack to prep for front-end work

---
+++ web/README.md
@@ -0,0 +1,9 @@
+Si32 Web Frontend
+========
+
+## Running
+From this directory, `docker-compose build` and then `docker-compose up` should
+run the front end on your machine at `localhost:4000`.
+
+Any changes to code inside `/src` should propagate through without needing
+to rebuild the image.  
---
+++ web/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    host: '0.0.0.0',
+    port: 4000
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+       }
+     ]
+   }
+}
