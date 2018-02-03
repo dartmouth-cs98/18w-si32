951d7807b24917dbc682caae9247f3b1dbe0bc83
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2018-02-03 16:03:18 -0500

    updated to work on docker as well

--- worker/bot1/bot.py
+++ worker/bot1/bot.py
@@ -1,7 +1,12 @@
 import sys
 import time
-print("I'm bot1.py")
+
+print("I'm bot1.py asjs")
 sys.stdout.flush()
-# l = sys.stdin.readline()
-print("bot1 received:")
-sys.stdout.flush()
+
+i = 0
+while True:
+    l = sys.stdin.readline()
+    print(str(i) + ": bot 1 received: " + l,)
+    i += 1
+    sys.stdout.flush()
