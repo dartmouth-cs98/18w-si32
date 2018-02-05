ed3a722eb6e4444650f2658eb78603b8c9ad4940
Gabe Boning <gboning@gmail.com>
2018-02-05 17:10:41 -0500

    shell of web UI

--- web/src/components/layout/page.js
+++ web/src/components/layout/page.js
@@ -3,18 +3,14 @@ import Radium from "radium";
 
 class Page extends React.PureComponent {
   render() {
-    return (
-      <div style={[styles.base]}>
-        { this.props.children }
-      </div>
-    );
+    return <div style={[styles.base]}>{this.props.children}</div>;
   }
 }
 
 const styles = {
   base: {
     maxWidth: 1080,
-    margin: '0px auto',
+    margin: "0px auto"
   }
 };
 
