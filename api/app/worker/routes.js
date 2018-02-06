1f91d9202e1b3d61678f14951b7ea747f89048b0
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2018-02-05 22:30:39 -0500

    api serving python file

---
+++ api/app/worker/routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require('path');
+const auth = require("../auth");
+const workerRouter = express.Router();
+
+// every bot route requires login
+workerRouter.use(auth.workerAuth);
+
+workerRouter.get("/nextTask", (req, res) => {
+  if (Math.random() < .5) {
+    res.send({ newGame: false, message: 'No Game Ready', players: [] });
+  } else {
+    res.send({ newGame: true, players: [1, 2] });
+  }
+});
+
+workerRouter.get("/file/:id", (req, res) => {
+  // TODO: this should obviously be fetching a bot from the database, not one stored in the file system.
+  res.sendFile(`bot${req.params.id}.py`, {root: path.join(__dirname, '../../public')})
+  // res.sendFile('index.html', { root: __dirname });
+});
+
+module.exports = workerRouter;
