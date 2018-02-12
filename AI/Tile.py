de1d0a33c079bc10af74d7ba9f4d7ed2f4819754
tongxu1996 <35202741+tongxu1996@users.noreply.github.com>
2018-02-12 14:30:18 -0500

    Add files via upload

--- AI/Tile.py
+++ AI/Tile.py
@@ -1,8 +1,21 @@
-from random import *
+from random import randint
+
+
 class Tile:
     def __init__(self, position):
         self.position = position
-        self.resource = randint(0, 9) #indicates the amount of resource contained by the tile (will be random?)
 
-    def decrement_resource(self): #reduces the resource (i.e. when a unit mines the resource)
+        self.resource = randint(0, 10) #amount of resource in the tile (will be randomized for now)
+        #TODO: implement symmetrical distribution of resource across map
+
+        self.units_A = []
+        self.units_B = []
+
+        self.building = None
+
+
+    def decrement_resource(self): #reduces the resource in the tile (i.e. when a unit mines the resource)
         self.resource -= 1
+
+    def __str__(self):
+        return "Units A at " + str(self.position) + ":" + str(self.units_A) + "\n" + "Units B at " + str(self.position) + ":" + str(self.units_B)
