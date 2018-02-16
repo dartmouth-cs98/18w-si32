85c51d035ce1fd5fb1457c69c78d56a8d90ed6e5
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2018-02-15 20:50:07 -0500

    random ocd stuff

--- web/src/style/constants.js
+++ web/src/style/constants.js
@@ -6,8 +6,9 @@ const BUTTON_HEIGHT = 35;
 // TODO: add font sizes to this to keep consistent across app?
 // would we want them to be absolute (px, pt) or relative (vw)?
 
+// TODO: decide on actual color palette, just here as placeholders 
 const PALETTE_PRIMARY = "#ec0b43";       // red
-const PALETTE_DETAIL = "#56666b";      // gray
+const PALETTE_DETAIL = "#56666b";        // gray
 const PALETTE_BACKGROUND = "#FFFFFF";    // white
 
 export {
