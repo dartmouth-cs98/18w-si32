315e15863b7a5ff15d8d3b91ab123513c41b7fb1
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2018-02-18 21:20:20 -0500

    separate components to handle file reading and file viz

---
+++ web/src/components/replay/ReplayVisualizer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ReplayVisualizer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>viz here</div>
+    );
+  }
+}
+
+export default ReplayVisualizer;
--- web/src/components/replay/replayPage.jsx
+++ web/src/components/replay/replayPage.jsx
@@ -4,15 +4,15 @@ import Color from "color";
 
 import Page from "../layout/page";
 
-import { colors, constants } from "../../style"
+import ReplayReader from "./ReplayReader";
+import ReplayVisualizer from "./ReplayVisualizer";
 
 class ReplayPage extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      contents: null,
-      dragOver: false
+      replay: null,
     };
   }
 
@@ -22,174 +22,25 @@ class ReplayPage extends React.PureComponent {
     if (!window.File || !window.FileReader) {
       console.log("FILE API ERROR");  // eslint-disable-line
     }
-
-    this.reader = this.setupFileReader();
   }
 
-  componentDidMount() {
-    const dropZone = document.getElementById('dropZone');
-
-    dropZone.addEventListener('dragenter', this.handleDragEnter, false);
-    dropZone.addEventListener('dragleave', this.handleDragLeave, false);
-    dropZone.addEventListener('dragover', this.handleDragOver, false);
-    dropZone.addEventListener('drop', this.handleFileSelect, false);
-  }
-
-  componentWillUnmount() {
-    const dropZone = document.getElementById('dropZone');
-
-    dropZone.removeEventListener('dragenter', this.handleDragEnter, false);
-    dropZone.removeEventListener('dragleave', this.handleDragLeave, false);
-    dropZone.removeEventListener('dragover', this.handleDragOver, false);
-    dropZone.removeEventListener('drop', this.handleFileSelect, false);
-  }
-
-  setupFileReader = () => {
-    const reader = new FileReader();
-    reader.onload = (f) => {
-      this.setState({
-        contents: f.target.result
-      });
-    };
-
-    return reader;
-  };
-
-  handleDragEnter = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-
-    this.setState({ dragOver: true });
-  };
-
-  handleDragLeave = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-
-    this.setState({ dragOver: false });
-  };
-
-  handleDragOver = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-
-    event.dataTransfer.dropEffect = "copy";
-  };
-
-  handleFileSelect = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-
-    let file;
-    if (event.dataTransfer) {
-      // if this is a drag / drop event
-      file = event.dataTransfer.files[0];
-
-    } else {
-      // if this is a traditional input event
-      file = event.target.files[0];
-    }
-
-    this.reader.readAsText(file, "utf-8");
-  };
-
-  renderFileSelectButton() {
-    return (
-      <div style={styles.buttonContainer}>
-        <input type="file"
-               name="file"
-               id="file"
-               style={styles.hiddenInput}
-               onChange={this.handleFileSelect}
-               name="files[]" />
-         <label style={styles.selectButton}
-                htmlFor="file">
-                Choose a file
-         </label>
-      </div>
-    );
+  setReplayFile = (f) => {
+    this.setState({ replay: f });
   }
 
   render() {
-    const dropZoneCnd = this.state.dragOver ? styles.dropZoneLit : {}
-    const fileSelectButton = this.state.dragOver ? null : this.renderFileSelectButton();
+    const main = this.state.replay ? <ReplayVisualizer /> : <ReplayReader setReplayFile={this.setReplayFile} />;
 
     return (
       <Page>
-        <div style={styles.wrapper}>
-          <div style={styles.uploadHeader}>Replay Your Bot</div>
-
-          <div style={{...styles.dropZoneBase, ...dropZoneCnd}} id="dropZone">
-            {fileSelectButton}
-            Drop a Replay File to Upload
-          </div>
-
-        </div>
+        {main}
       </Page>
     );
   }
 }
 
 const styles = {
-  wrapper: {
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingTop: "40px"
-  },
-  uploadHeader: {
-    color: colors.primary,
-    fontSize: "30px"
-  },
-  dropZoneBase: {
-    width: "60%",
-    height: "400px",
-    borderStyle: "solid",
-    borderRadius: "2px",
-    borderColor:  Color(colors.detail).lighten(0.7).string(),
-    borderWidth: "1px",
-    margin: "20px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  dropZoneLit: {
-    backgroundColor: Color(colors.primary).lighten(0.9).string()
-  },
-  buttonContainer: {
-    width: "50%"
-  },
-  hiddenInput: {
-    width: "0.1px",
-    height: "0.1px",
-    opacity: "0",
-    overflow: "hidden",
-    position: "absolute",
-    zIndex: "-1"
-  },
-  selectButton: {
-    width: "100%",
-    height: constants.BUTTON_HEIGHT,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    margin: "15px 0",
-    backgroundColor: colors.background,
-    color: colors.primary,
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderColor: colors.primary,
-    borderRadius: "2px",
-    ":hover": {
-      backgroundColor: colors.primary,
-      color: colors.background,
-      cursor: "pointer"
-    }
-  }
+
 };
 
 export default connect(null, null)(ReplayPage);
