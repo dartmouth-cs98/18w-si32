789289599984e49adf2e946c4728a6ef46dbd788
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2018-02-22 16:01:42 -0500

    paging on leaderboard table

--- web/src/components/leaderboard/leaderboardPage.jsx
+++ web/src/components/leaderboard/leaderboardPage.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import _ from "lodash";
 import { connect } from "react-redux";
 
 import Page from "../layout/page";
-import Link from "../common/link";
 import groupSearchbar from "../groups/groupSearchbar";
 import LeaderboardTable from "./LeaderboardTable";
 
@@ -11,13 +9,6 @@ import capitalize from "../../util/capitalize";
 
 import { MainTitle, SubTitle } from "../common/titles";
 import { fetchLeaderboard } from "../../data/leaderboard/leaderboardActions";
-
-const RankedList = ({ users }) => {
-  console.log(users);
-  return _.map(users, (u) => (
-    <Link key={u._id} href={`/users/${u._id}`}>{u.username} ({u.trueSkill.mu.toFixed(1)})</Link>
-  ));
-};
 
 class LeaderboardPage extends React.PureComponent {
   constructor(props) {
@@ -60,6 +51,7 @@ class LeaderboardPage extends React.PureComponent {
     const groupId = this.state.selectedGroup ? this.state.selectedGroup.value : "global";
     const leaderboard = this.props.leaderboards[groupId] || {users: []};
     const users = leaderboard.users;
+    const numPages = leaderboard.numPages;
 
     return (
       <Page>
@@ -67,7 +59,7 @@ class LeaderboardPage extends React.PureComponent {
           <MainTitle>Leaderboard</MainTitle>
           <SubTitle>{groupLabel}</SubTitle>
           {groupSearchbar(this.state.selectedGroup, this.didSelectGroup, {placeholder: "Choose A Specific Group"})}
-          <LeaderboardTable users={users} totalPages={2} loading={false} fetchPage={this.onFetchPage} />
+          <LeaderboardTable users={users} totalPages={numPages} loading={false} fetchPage={this.onFetchPage} />
         </div>
       </Page>
     );
