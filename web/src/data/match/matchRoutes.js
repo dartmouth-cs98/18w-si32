9c5d4b8c2b2e27e0ddaf465d1c450dd583f0821a
Gabe Boning <gboning@gmail.com>
2018-02-25 00:07:00 -0500

    fix lint

--- web/src/data/match/matchRoutes.js
+++ web/src/data/match/matchRoutes.js
@@ -2,7 +2,7 @@ import request from "superagent";
 import msgpack from "msgpack-lite";
 
 const fetchLog = (logUrl) => {
-  return request.get(logUrl).responseType('arraybuffer')
+  return request.get(logUrl).responseType("arraybuffer")
           .then(res => msgpack.decode(new Uint8Array(res.body)));
 };
 
