d3a2239ab3726bebceea7075db381aa68d4c933b
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2018-03-03 22:01:11 -0500

    script for creating boilerplate sample bot

---
+++ sample_bots/new5/bot.py
@@ -0,0 +1,28 @@
+import sys
+import time
+from GameHelper import GameHelper
+
+game = GameHelper()
+
+
+while True:
+    # l = sys.stdin.readline()
+    # print(str(i) + ": bot 2 received: " + l,)
+
+    commands = []
+
+    # load state for next turn
+    game.load_state()
+
+    units = game.get_my_units()
+
+    buildings = game.enemy_buildings()
+    if len(buildings) > 0:
+        for s in units:
+            m = game.move_towards(s.position,buildings[0].position)
+            if m:
+                commands.append(m)
+
+
+    # done for this turn, send all my commands
+    game.send_commands(commands)
