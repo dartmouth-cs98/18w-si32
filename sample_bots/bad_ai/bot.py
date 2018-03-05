97bd86afd93960049dc606e97b6d95b6a56343e3
tongxu96 <tong.xu.18@dartmouth.edu>
2018-03-05 15:55:48 -0500

    add bot

---
+++ sample_bots/bad_ai/bot.py
@@ -0,0 +1,46 @@
+import sys
+import time
+from GameHelper import GameHelper
+
+game = GameHelper()
+
+
+while True:
+    # l = sys.stdin.readline()
+    # print(str(i) + ": bot 2 received: " + l,)
+
+    commands = []
+
+    # load state for next turn
+    game.load_state()
+
+    tiles = self.GameHelper.get_occupied_tiles(playerId)
+
+    for tile in tiles:
+        # if dominant in units and buildings, go on the offensive
+        if self.GameHelper.compare_total_units(game.myId, game.eId) & self.GameHelper.compare_building_count(
+                game.myId, game.eId) & self.GameHelper.compare_resource(game.myId, game.eId):
+
+            (nearest_enemy_building_position,
+             distance) = self.GameHelper.get_nearest_building_position_and_distance_belonging_to_player(
+                tile.position[0], tile.position[1], playerId)
+
+            # only head towards the buildings with defense weaker than the number of units you control on this tile
+            if (self.GameHelper.get_nearest_building_position_and_distance_belonging_to_player(
+                tile.position[0], tile.position[1], playerId) is not None) & self.GameHelper.get_tile(nearest_enemy_building_position[0],
+                                        nearest_enemy_building_position[1]).building.defense < tile.units[playerId]:
+
+                commands.append(self.GameHelper.single_move_towards_tile_avoiding_things(playerId, tile.position,
+                                                                                         nearest_enemy_building_position,
+                                                                                         tile.units[playerId],
+                                                                                         'stronger_buildings'))
+            else:
+                commands += self.GameHelper.efficient_mine_and_build(playerId, tile.position)
+        # otherwise focus on resource gathering and building
+        else:
+
+            commands += self.GameHelper.efficient_mine_and_build(playerId, tile.position)
+
+
+    # done for this turn, send all my commands
+    game.send_commands(commands)
