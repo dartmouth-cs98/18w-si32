7e23b5a60e6719e73025be1d74d81428c837da65
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2018-03-05 21:35:20 -0500

    whoops

--- web/src/favicon/favicon.js
+++ web/src/favicon/favicon.js
@@ -1,7 +1,7 @@
 // Favicon webpack config courtesy of Olivier Gonzalez
 // https://medium.com/tech-angels-publications/bundle-your-favicons-with-webpack-b69d834b2f53
 const faviconsContext = require.context(                 // eslint-disable-line
-  "!!file-loader?name=assets/favicons/[name].[ext]!.",
+  "!!file-loader?name=assets/favicon/[name].[ext]!.",
   true,
   /\.(svg|png|ico|xml|json)$/
 );
