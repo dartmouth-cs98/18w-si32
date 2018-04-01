0490ec2ca12f49d86a4e18ea308d9de757c51b02
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2018-03-31 22:15:50 -0400

    command

--- game/Command.py
+++ game/Command.py
@@ -1,22 +1,25 @@
+# Command.py
+# Class definition for 'Command'
+
 from game.Tile import Tile
 from game.Coordinate import Coordinate
 
-class Command:
+# Constructor Arguments:
+# playerID         -  the ID of the player issuing this command.
+# position         -  the board position in which this command originates.
+# command          -  the type of command issued.
+# number_of_units  -  the number of units involved in this command.
+# direction        -  the direction associated with this command.
 
-    # location is tuple of square, direction is tuple of which direction
+class Command:
     def __init__(self, playerId, position, command, number_of_units, direction):
         self.playerId = playerId
-        self.position = Coordinate(position)
+
         self.command = command
+
+        self.direction = direction              # direction is tuple
+        self.position = Coordinate(position)    # position is tuple 
         self.number_of_units = number_of_units
-        self.direction = direction
-
-    def decrement_units(self, number):
-        if number <= self.number_of_units:
-            self.number_of_units -= number
-
-    def increment_units(self, number):
-        self.number_of_units += number
 
     @classmethod
     def from_dict(cls, playerId, d):
