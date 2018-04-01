2aa27dce9cccc4b1bad21a398e78b4fb8c42c829
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2018-04-01 15:28:34 -0400

    coordinate

--- game/Command.py
+++ game/Command.py
@@ -5,11 +5,11 @@ from game.Tile import Tile
 from game.Coordinate import Coordinate
 
 # Constructor Arguments:
-# playerID         -  the ID of the player issuing this command.
-# position         -  the board position in which this command originates.
-# command          -  the type of command issued.
-# number_of_units  -  the number of units involved in this command.
-# direction        -  the direction associated with this command.
+# playerID          -  the ID of the player issuing this command.
+# position (tuple)  -  the board position in which this command originates.
+# command           -  the type of command issued.
+# number_of_units   -  the number of units involved in this command.
+# direction (tuple) -  the direction associated with this command.
 
 class Command:
     def __init__(self, playerId, position, command, number_of_units, direction):
@@ -17,8 +17,8 @@ class Command:
 
         self.command = command
 
-        self.direction = direction              # direction is tuple
-        self.position = Coordinate(position)    # position is tuple 
+        self.direction = direction
+        self.position = Coordinate(position)
         self.number_of_units = number_of_units
 
     @classmethod
--- game/Coordinate.py
+++ game/Coordinate.py
@@ -1,3 +1,11 @@
+# Coordinate.py
+# Class definition for 'Coordinate'
+
+# Constructor Arguments
+# (tuples) - the tuple that is, essentially, cast to a Coordinate type.
+
+# TODO: why is y first and x second in the coordinate?? 
+
 class Coordinate(tuple):
     @property
     def x(self):
@@ -7,12 +15,12 @@ class Coordinate(tuple):
     def y(self):
         return self[0]
 
+    """ Returns the vector addition of self and other """
     def __add__(self, other):
-        """ Returns the vector addition of self and other """
         other = Coordinate(other)
         return Coordinate((self.y + other.y, self.x + other.x))
 
+    """ Returns the vector difference of self and other """
     def __sub__(self, other):
-        """ Returns the vector difference of self and other """
         other = Coordinate(other)
         return Coordinate((self.y - other.y, self.x - other.x))
