1c3cc3a72fc3c7ea9bae5b2a90828651dd88936e
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2018-03-31 22:06:20 -0400

    building

--- game/Building.py
+++ game/Building.py
@@ -1,25 +1,21 @@
+# Building.py
+# Class definition for 'Building'
+
 from game.params import PRODUCTION_RATE, DEFENSE_RATING
 
-nextId = 0
+# Constructor Arguments
+# ownerID: The unique ID of this building's owner.
 
 class Building:
     def __init__(self, ownerID):
-        global nextId
-        self.ownerId = ownerID         # ID of the player who controls this building
-
-        self.production_progress = 0
+        self.ownerId = ownerID
+        self.production = 0
         self.defense = DEFENSE_RATING
-        self.buildingID = nextId
-        nextId += 1
 
-    def update_production_status(self):
-        self.production_progress += PRODUCTION_RATE
-
-    def increment_production_progress(self, number):  #increase production count by how many workers are on the same tile
-        self.production_progress += number
-
-    def decrement_production(self, number=10):
-        self.production_progress -= number
+    # Increment the production value of this building
+    # by the default production rate. 
+    def increment_production(self):
+        self.production += PRODUCTION_RATE
 
     def __str__(self):
-        return ("BUILDING: " + str(self.buildingID) + "; OWNER: " + str(self.ownerId))
+        return ("BUILDING OWNER: " + str(self.ownerId))
