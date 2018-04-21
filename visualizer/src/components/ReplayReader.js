c9f9f4a7a42c658b403f0c2244ffa5f34b22a6ed
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2018-04-21 13:32:17 -0400

    parsing file, rendering replay

--- visualizer/src/components/ReplayReader.js
+++ visualizer/src/components/ReplayReader.js
@@ -6,9 +6,22 @@ const fs = window.require("fs");
 const { dialog } = window.require("electron").remote;
 
 class ReplayReader extends PureComponent {
+  // TODO: more rigorous file verification
+  verifyReplayFile = (json) => {
+    if (!json.w || !json.h) {
+      // must have width and height properties
+      return false;
+    } else if (!json.turns || !Array.isArray(json.turns)) {
+      // turns array must be present
+      return false;
+    }
+    return true;
+  };
+
   openFileDialog = () => {
     dialog.showOpenDialog((filenames) => {
       if (filenames === undefined) {
+        // TODO: inform user
         return;
       }
 
@@ -18,8 +31,22 @@ class ReplayReader extends PureComponent {
           // TODO: alert user to bad file, or other error when reading
           return;
         }
-        // got the file data, so set the replay file to contents
-        this.props.setReplayFile(data);
+        let result;
+        try {
+          result = JSON.parse(data);
+        } catch(SyntaxError) {
+          // thrown by failed JSON.parse()
+          // TODO: inform user
+          return;
+        }
+
+        if (this.verifyReplayFile(result)) {
+          // good to go
+          this.props.setReplayFile(result);
+        } else {
+          // TODO: inform user
+          return;
+        }
       });
     });
   }
