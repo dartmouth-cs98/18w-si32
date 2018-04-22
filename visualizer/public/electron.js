eb4064d944bdea85eb765a8776eb985bfd3465ec
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2018-04-21 21:01:28 -0400

    doc updates

--- visualizer/public/electron.js
+++ visualizer/public/electron.js
@@ -14,7 +14,11 @@ const isDev = require("electron-is-dev");
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    icon: path.join(__dirname, "icons/png/64x64.png")
+   });
   mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
   mainWindow.on("closed", () => mainWindow = null);
 
