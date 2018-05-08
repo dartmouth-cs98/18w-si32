0c3a69be28997197a440e8671943feff07d2f44a
Gabe Boning <gboning@gmail.com>
2018-05-07 21:07:13 -0400

    add other bot volumes for live worker

--- worker/startLive.sh
+++ worker/startLive.sh
@@ -3,5 +3,7 @@ docker run -i  \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v bot0:/bot0 \
   -v bot1:/bot1 \
+  -v bot2:/bot2 \
+  -v bot3:/bot3 \
   --env api_route=https://lit-mountain-10578.herokuapp.com/worker \
   si32-worker
