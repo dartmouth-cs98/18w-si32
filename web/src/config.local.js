ff66153d974ede1233779d5c9c70deff6bf1d129
Gabe Boning <gboning@gmail.com>
2018-05-10 18:29:40 -0400

    reset local config

--- web/src/config.local.js
+++ web/src/config.local.js
@@ -1,5 +1,5 @@
 export default {
-  API_URL: "https://lit-mountain-10578.herokuapp.com",
+  API_URL: "http://localhost:3000",
   DOCS_URL: "http://localhost:4001",
   FEEDBACK_URL: "https://monadfeedback.typeform.com/to/WqIjVe",
   MIXPANEL_TOKEN: "51172a1996a5e6e2714b3b2050c446d8",
