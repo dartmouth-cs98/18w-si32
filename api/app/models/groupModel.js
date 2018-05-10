636b6ef383c3fa580e0b972188c27b430e091d0c
Gabe Boning <gboning@gmail.com>
2018-05-09 22:12:48 -0400

    remove unused import

--- api/app/models/groupModel.js
+++ api/app/models/groupModel.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const User = require("./userModel");
-const ensureRequiredProps = require("../lib/ensureRequiredProps");
 
 const { MalformedError } = require("../errors");
 
