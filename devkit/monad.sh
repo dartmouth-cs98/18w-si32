692193c2816e039b07735f125bbc8d2f0e937610
Kyle Dotterrer <kyle.dotterrer.18@dartmouth.edu>
2018-05-17 17:55:54 -0400

    new script to handle all dev commands

---
+++ devkit/monad.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# check if a command was even specified
+if [ "$#" -lt 1 ]; then
+  echo "usage: monad.sh command param1 [param2 ...]"
+  exit 1
+fi
+
+if [ "$1" = "match" ]; then
+  # run the match script
+  python ./scripts/match.py "${@:2}"
+elif [ "$1" = "train" ]; then
+  # run the train script
+  python ./scripts/train.py "${@:2}"
+elif [ "$1" = "upload" ]; then
+  # run the upload script
+  python ./scripts/upload.py "${@:2}"
+else
+  # unrecognized command
+  echo "ERROR: unrecognized command"
+  exit 1
+fi
+
+exit 0
